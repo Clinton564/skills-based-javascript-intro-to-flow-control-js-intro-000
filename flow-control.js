4ba25226e6f09953a4527bcbf5195b138c6c82f6
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-20 00:37:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,30 @@
 function basicTeenager(age) {
-
+  if(age > 12 && age < 20)
+  {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if(age > 12 && age < 20)
+  {
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if(age < 13) {
+    return "You are a kid"
+  }
+  else if(age > 19) {
+    return "You are a grownup"
+  }
+  else {
+    return "You are a teenager!"
+  }
 }
 
 function ternaryTeenager(age) {
